82b724d975f0e78dcf260bd3a9c17f70e91bc5b5
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-28 18:19:29 -0300

    feat: criando função para repetir a string

---
+++ repeat/repeatString.js
@@ -0,0 +1,11 @@
+const repeatStr = (n, s) => {
+    let x = '';
+    for( let i = 0; i < n; i++){
+      x += s;
+    }
+    console.log(x);
+    return x;
+  }
+
+  // criando função que receba dois parametros 1 numero e uma string
+  // e repita a string na quantidade de vezs = ao numero
